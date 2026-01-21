9844d76691f44f19548416f22c50f7708870fb1d
abuhuraira-73 <abuhuraira1514@gmail.com>
2026-01-21 12:06:07 +0530

    refined footer

--- VS-portfolio-2026/Controllers/HomeController.cs
+++ VS-portfolio-2026/Controllers/HomeController.cs
@@ -33,6 +33,18 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Service()
+    {
+        return View();
+    }
+
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
