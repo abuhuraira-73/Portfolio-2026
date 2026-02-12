a4dc117179edad268e48a77bcc7c806973fb5a2e
abuhuraira-73 <abuhuraira1514@gmail.com>
2026-02-12 18:03:16 +0530

    Created a portfolio controller for individual portfolio pages

---
+++ VS-portfolio-2026/Controllers/PortfolioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using VS_portfolio_2026.Models;
+
+namespace VS_portfolio_2026.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Chromaic()
+        {
+            return View();
+        }
+    }
+}
+
